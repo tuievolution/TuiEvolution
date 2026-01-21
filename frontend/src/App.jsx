fd5f75fd206e7c531f081c642dfa6b892620ae71
tuanaakyildiz <139168702+tuanaakyildiz@users.noreply.github.com>
2026-01-22 01:27:00 +0300

    Update App.jsx
    trying to set-up router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {AboutUs, Main, Projects} from './pages'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <NavBar/>
-    </>
+    <BrowserRouter>
+      <header>
+        <NavBar/>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Main/>} />
+          <Route path='/AboutUs' element={<AboutUs/>} />
+          <Route path='/Projects' element={<Projects/>} />
+        </Routes>
+      </main>
+
+
+    </BrowserRouter>
   )
 }
 
