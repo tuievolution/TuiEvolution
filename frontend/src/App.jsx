3edb5585d1e0e2fb5ceb68e2f676f3c7ac7fd130
tuanaakyildiz <139168702+tuanaakyildiz@users.noreply.github.com>
2026-02-04 18:05:42 +0300

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Container } from '@mui/material';
-import AppRouter from './router';
-import NavBar from './components/NavBar';
-import { navRoutes } from './router/routes';
+import AppRouter from './router/index.jsx'; // index.jsx olduğundan emin olun
+import NavBar from './components/NavBar.jsx';
+import { navRoutes } from './router/routes.jsx';
 
 function App() {
   return (
+    // BrowserRouter AppRouter içinde olduğu için burada tekrar kullanmıyoruz
     <>
       <NavBar routes={navRoutes} />
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
