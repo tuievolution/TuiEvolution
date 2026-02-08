6a1167500f06a312706e19a7fc3d5c4f6eee9282
tuanaakyildiz <139168702+tuanaakyildiz@users.noreply.github.com>
2026-02-08 20:25:52 +0300

    Update UserRepository.java

--- backend/src/main/java/com/tuievolution/repository/UserRepository.java
+++ backend/src/main/java/com/tuievolution/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.tuievolution.repository;
 
-import com.tuievolution.model.User; // User modelini import etmelisin
-import org.springframework.data.jpa.repository.JpaRepository; // JpaRepository için şart
+import com.tuievolution.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
