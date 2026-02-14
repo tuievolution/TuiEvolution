3bee4ab76d5f7abd9ef258eceec82540409663ea
tuanaakyildiz <139168702+tuanaakyildiz@users.noreply.github.com>
2026-02-14 03:02:54 +0300

    Update WebConfig.java

--- backend/src/main/java/com/tuievolution/config/WebConfig.java
+++ backend/src/main/java/com/tuievolution/config/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // "*" yerine "allowedOriginPatterns" kullanın
+                .allowedOriginPatterns("*") // "allowCredentials" true iken "*" yerine pattern kullanılmalıdır
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true); // Bu true olduğu için üstteki pattern gereklidir
+                .allowCredentials(true);
     }
 }
